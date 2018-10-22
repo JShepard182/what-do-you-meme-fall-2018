9258106e2238af012faf354845c81bdecb468d3d
Justin Shepard <justinshepard182@gmail.com>
2018-10-22 12:10:47 -0400

    Used Vue to make pages look nicer and change navigation menu

---
+++ src/views/Game.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="alert alert-success">
+        Yay We Have A Game!
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
+
+<script>
+export default {
+    
+}
+</script>
+
+
